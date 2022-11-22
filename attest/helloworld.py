47696730b112c1167362990d7a90fbd2a6763a2b
nathro <nathro.software@gmail.com>
2022-11-21 17:40:09 -0800

    Initial Setup

---
+++ attest/helloworld.py
@@ -0,0 +1,18 @@
+# @black_format
+
+
+class HelloWorld:
+    def print_hello(self):
+        print("Hello World!")
+
+    def print_goodbye(self):
+        print("Goodbye!")
+
+
+
+
+
+if __name__ == "__main__":
+    world = HelloWorld()
+    world.print_hello()
+    world.print_goodbye()
